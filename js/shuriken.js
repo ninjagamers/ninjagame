7394dfa325ffab418937dce797a49fee73a9d046
kayla-strezovski <kstrez8@internode.on.net>
2015-10-21 19:17:25 +1030

    basic shuriken and enemy code added
    animation implemented to old structure

---
+++ js/shuriken.js
@@ -0,0 +1,24 @@
+
+var Shuriken = function (x, y)
+{
+	this.sprite = new Sprite ("../shuriken_sprite/.png");
+	this.sprite.buildAnimation (2, 1, 64, 64, 0.05, [0,1]);
+	this.sprite.setAnimationOffset(0, 0, 0);
+	
+	this.position = new Vector2();
+	this.position.set(x,y);
+	
+	this.velocity = new Vector2();
+}
+
+Shuriken.prototype.update = function(deltaTime)
+{
+	this.sprite.update(deltaTime);
+	this.position.x = Math.floor(this.position.x + (deltaTime * -this.velocity.x))
+}
+
+Shuriken.prototype.draw = function()
+{
+	var screenX = this.position.x - worldOffsetX;
+	this.sprite.draw(context, screenX, this.position.y);
+}
