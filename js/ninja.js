3f8f9c83fd0555bed097fe92e6183c108e60e4ac
kayla-strezovski <kstrez8@internode.on.net>
2015-10-21 19:08:10 +1030

    player.js and sprite updated and added

--- js/ninja.js
+++ js/ninja.js
@@ -1,14 +1,34 @@
+
+//SPRITES
+//running on floor
+var ANIM_RUN_FLOOR = 0;
+//running on roof
+var ANIM_RUN_ROOF = 1;
+// die/ live lost - on floor
+var ANIM_DIE_FLOOR = 2;
+// die/ live lost - on roof
+var ANIM_DIE_ROOF = 3;
+var ANIM_MAX = 4;
+
 var Ninja = function(){
 
-  this.sprite = new Sprite("tiles/ninjasprites.png");
-  this.sprite.buildAnimation(5, 3, 32, 32, 0.05,
-    [5, 6]);
+  this.sprite = new Sprite("images/player_sprite.png");
+  
+  this.sprite.buildAnimation(5, 10, 64, 64, 0.05,[1, 2, 3, 4]);
+  this.sprite.buildAnimation(5, 10, 64, 64, 0.05,[47, 48, 49 ,50]);
+  this.sprite.buildAnimation(5, 10, 64, 64, 0.05,[17]);
+  this.sprite.buildAnimation(5, 10, 64, 64, 0.05,[32]);
 
+  for (var i=0; i<ANIM_MAX; i++)
+	{
+		this.sprite.setAnimationOffset(i, - 55, -87);
+	}
+  
   this.position = new Vector2();
   this.position.set(TILE * 2, TILE * 5);
 
-  this.width = TILE;
-  this.height = TILE;
+  this.width = 64;
+  this.height = 64;
 
   this.gravityDown = true;
   this.lives = 3;
