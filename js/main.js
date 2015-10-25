728584321f26d17ee66415b2c0e0ec0802622d1a
Andrew Dunn <andrewjamesdunn@gmail.com>
2015-10-25 14:34:30 +1030

    Setup initial functionality of randomly picking levels and moving through them forever.

--- js/main.js
+++ js/main.js
@@ -27,11 +27,15 @@ function getDeltaTime()
 var SCREEN_WIDTH = canvas.width;
 var SCREEN_HEIGHT = canvas.height;
 
+// Load the image to use for level tiles.
+var tileset = document.createElement("img");
+tileset.src = "tiles/tiles2.png";
+
 // Tile set constants.
 var TILE = 32; // Width/height of tile.
 var MAP = {tw:20, th:15};
-var TILESET_COUNT_X = 5; // Columns in the tileset.
-var TILESET_COUNT_Y = 3; // Rows in the tileset.
+var TILESET_COUNT_X = 4; // Columns in the tileset.
+var TILESET_COUNT_Y = 2; // Rows in the tileset.
 var TILESET_TILE = TILE *2;
 
 // Forces constants.
@@ -39,32 +43,94 @@ var METER = TILE; // 1m.
 var GRAVITY = METER * 9.8; // Default gravity.
 var INIT_SCREEN_MOVEMENT_SPEED = TILE;
 
+// Level position
+var stageOffsetX = 0;
+
 // Other
 var ninja = new Ninja();
 var keyboard = new Keyboard();
+var courses = []; // levels
 
 
 function init()
 {
-  // Main constructor.
+    // Main constructor.
+    addRandomCourse();
+    addRandomCourse();
 }
 
 function run()
 {
-    var deltaTime = getDeltaTime();
-    context.fillStyle = "#ccc";
+    var deltaTime = getDeltaTime(); // Get Delta.
+    context.fillStyle = "#ccc"; // Clear Screen.
     context.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    // Add Game logic here.
+    // Add Game logic here:
+
+
+    // Move the level.
+    stageOffsetX = stageOffsetX + (deltaTime * 150);
+
+    // Handle levels
+
+    // Draw ALL the maps.
+    destroyCourses();
+
+    var index;
+    for (index = 0; index < courses.length; ++index) {
+        drawMap(courses[index], 0, Math.floor(stageOffsetX - (index * (20 * TILE))));
+        drawMap(courses[index], 1, Math.floor(stageOffsetX - (index * (20 * TILE))));
+    }
 
     // Handle Ninja.
     // ninja.update();
     ninja.draw();
-
 }
 
-function drawMap()
+function drawMap(test, drawlayer, stageOffsetXx)
 {
+    // Scrolling the level.
+    var maxTiles = 20;
+
+    // Draw the platform Map.
+    for(var y = 0; y < 15; y++)
+    {
+        var idx = y * test.layers[drawlayer].width;
+        for(var x = 0; x < maxTiles; x++)
+        {
+            if(test.layers[drawlayer].data[idx] != 0)
+            {
+                // Tiled map are base 1, so subtract one from the tileset id to get the correct tile.
+                var tileIndex = test.layers[drawlayer].data[idx] - 1;
+                var sx = (tileIndex % TILESET_COUNT_X) * TILE;
+                var sy = (Math.floor(tileIndex / TILESET_COUNT_X)) * TILE;
+
+                // Draw Image: context.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+                context.drawImage(tileset, sx, sy, TILE, TILE, x * TILE - stageOffsetXx, (y) * TILE, TILE, TILE);
+            }
+            idx++;
+        }
+    }
+}
+
+// Adds a course.
+function addRandomCourse()
+{
+    // Randomly select a course from the all courses array and add it to the courses array.
+    var newcourse = allcourses[Math.floor(Math.random() * allcourses.length)];
+    courses.push(newcourse);
+}
+
+// Adds a course.
+function destroyCourses()
+{
+    if (stageOffsetX >= 640)
+    {
+        courses.shift();
+        stageOffsetX = stageOffsetX - 640;
+        addRandomCourse();
+    }
+
 }
 
 // Util Functions.
